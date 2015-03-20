f4d08bb6ab7cc3aa77762d6dde1c348725cd3f6b
darthneel <neelp22@gmail.com>
2015-03-20 17:13:00 -0400

    updates

--- index.js
+++ index.js
@@ -1,53 +1,96 @@
 var fs = require('fs');
-var _ = require('underscore');
-require('locus');
+
+function compareObjects(a, b) {
+
+  var aKeys = Object.keys(a);
+  var bKeys = Object.keys(b);
+
+  if (aKeys.length != bKeys.length) {
+    return false;
+  };
+
+  for (var i = 0; i < aKeys.length; i++) {
+    var property = aKeys[i];
+
+
+    if (a[property] !== b[property]) {
+      return false;
+    };
+  };
+
+  return true;
+};
 
 
 module.exports = {
-  save: function(filename, obj){
-    if(typeof(obj) != 'object'){
+  save: function(filename, obj) {
+    if (typeof(obj) != 'object') {
       console.log("Error: You can only save arrays and objects");
+
       return
     };
 
-    if(fs.existsSync(filename)){
+    if (!fs.existsSync(filename) || fs.readFileSync(filename).toString() === '') {
+      var data = [obj];
+      fs.writeFileSync(filename, JSON.stringify(data));
+
+      return data
+    } else {
       var data = JSON.parse(fs.readFileSync(filename));
       data.push(obj);
       fs.writeFileSync(filename, JSON.stringify(data));
+
       return data
-    }else{
-      var data = [obj];
-      fs.writeFileSync(filename, JSON.stringify(data));
-      return data
-    };
+    }
   },
-  retrieve: function(filename){
-    if(fs.existsSync(filename)){
+  retrieve: function(filename) {
+    if (fs.existsSync(filename)) {
       return JSON.parse(fs.readFileSync(filename));
-    }else{
+    } else {
       console.log("Error: File does not exist");
       return
-    };
+    }
   },
-  delete: function(filename, obj){
-    if(typeof(obj) != 'object'){
-      console.log("Error: You can only save arrays and objects");
-      return
-    };
+  delete: function(filename, obj) {
+    if (typeof(obj) != 'object') {
+      console.log("Error: You can only delete arrays and objects");
 
-    if(!fs.existsSync(filename)){
-      consle.log("Error: File does not exist")
       return
     }
 
+    if (!fs.existsSync(filename)) {
+      consle.log("Error: File does not exist");
+
+      return
+    }
+
+    console.log("hit");
+
     var data = JSON.parse(fs.readFileSync(filename));
 
-    var index = data.indexOf(obj);
-    index > 0 ? data.splice(index, 1) : console.log("Error: Object does not exist.")
+    var indexOfMatch;
+    data.forEach(function(el, index, array) {
+      if ( compareObjects(el, obj) ) {
+        indexOfMatch = index;
+      }
+    });
 
-    eval(locus);
+    typeof indexOfMatch === 'number' ? data.splice(indexOfMatch, 1) : console.log("Error: Object not found.")
+
+    fs.writeFile(filename, JSON.stringify(data), function(err){
+      if (err) {
+        console.log(err);
+      }
+    });
 
     return data
+  },
+  deleteAll: function(filename) {
+    fs.unlink(filename, function(err) {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
 }
 
